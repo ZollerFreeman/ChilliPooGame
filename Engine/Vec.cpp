fd3e1d384441702ba1c979c53651c7eab30d8d02
PaperMoon <zraketa24@freemail.hu>
2019-01-11 14:30:17 +0100

    Vector class normalization added, length querry added

--- Engine/Vec.cpp
+++ Engine/Vec.cpp
@@ -10,6 +10,16 @@ Vec & Vec::operator+=(const Vec & rhs)
 	return *this = *this + rhs;
 }
 
+Vec Vec::operator-(const Vec & rhs) const
+{
+	return Vec(x - rhs.x, y - rhs.y);
+}
+
+Vec & Vec::operator-=(const Vec & rhs)
+{
+	return *this = *this - rhs;
+}
+
 Vec Vec::operator*(const Vec & rhs) const
 {
 	return Vec(x * rhs.x, y * rhs.y);
@@ -29,3 +39,30 @@ Vec & Vec::operator*=(float val)
 {
 	return *this = *this * val;
 }
+
+float Vec::GetLength() const
+{
+	return std::sqrt(GetLengthSq());
+}
+
+float Vec::GetLengthSq() const
+{
+	return x * x + y * y;
+}
+
+Vec & Vec::Normalize()
+{
+	return *this = GetNormalized();
+}
+
+Vec Vec::GetNormalized() const
+{
+	const float length = GetLength();
+	if (length != 0.0f) { return *this * (1 / length); }
+	else { return *this; }
+
+	}
+
+
+
+
--- Engine/Vec.h
+++ Engine/Vec.h
@@ -1,18 +1,28 @@
 #pragma once
-
+#include <cmath>
 
 class Vec {
 public:
 	Vec() = default;
 	Vec(float x_in, float y_in) :x(x_in), y(y_in) {}
+
 	Vec operator + (const Vec& rhs) const;
 	Vec& operator += (const Vec& rhs);
+
+	Vec operator - (const Vec& rhs) const;
+	Vec& operator -= (const Vec& rhs);
 
 	Vec operator * (const Vec& rhs) const;
 	Vec& operator *= (const Vec& rhs);
 
 	Vec operator * (float val) const;
 	Vec& operator *= (float val);
+
+	float GetLength () const;
+	float GetLengthSq () const;
+
+	Vec& Normalize();
+	Vec GetNormalized() const;
 
 public:
 	float x;
